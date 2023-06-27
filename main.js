589ae26234fe14f8954a782228fb2bbdd6a033ca
Anavasconcelos79 <dianavasconcelos79@gmail.com>
2023-06-27 20:37:12 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('formulario');
+const nome = [];
+const numero = [];
+
+let linhas = " ";
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeCadastrado.value = "";
+    inputNumeroTelefone.value = "";
+
+    function atualizaTabela() {
+        const corpoTabela = document.querySelector("tbody");
+        corpoTabela.innerHTML = linhas;
+    }
+}
